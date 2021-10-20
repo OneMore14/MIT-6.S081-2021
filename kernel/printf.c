f34422be8b59d26a4de308d775f40efb18cc706f
om14 <om14>
2021-10-21 00:01:34 +0800

    backtrace done

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,6 +121,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -131,4 +132,22 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void 
+backtrace(void) {
+  
+  uint64 fp = r_fp();
+  uint64 stacktop = PGROUNDUP(fp);
+  uint64 *ra = (uint64*)(fp - 8);
+  while(1) {
+
+    printf("%p\n", *ra);
+
+    fp = *((uint64*)(fp - 16));
+    ra = (uint64*)(fp - 8);
+    if (fp == stacktop) {
+      break;
+    }
+  }
 }
